9f359055a96faa9f137b4e7689b8f7438eba1dbe
Steven <deltaorb637@gmail.com>
2020-12-13 00:32:45 -0500

    Created OrderedArrayList class.

---
+++ OrderedArrayList.java
@@ -0,0 +1,10 @@
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
+    public OrderedArrayList(){
+        super();
+    }
+    public OrderedArrayList(int size){
+        super(size);
+    }
+    
+
+}
