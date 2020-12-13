1de11ea8ecdfbc523f82497e70b6e578031c64f2
Steven <deltaorb637@gmail.com>
2020-12-13 02:03:28 -0500

    Overwrote methods, but still need to fix getSortIndex so it sorts properly.

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -5,6 +5,27 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     public OrderedArrayList(int size){
         super(size);
     }
+    public boolean add(T element){
+        super.add(getSortIndex(element), element);
+        return true;
+    }
+    public void add(int index, T element){
+        super.add(getSortIndex(element), element);
+    }
+    public T set(int index, T element){
+        T removed = super.get(index);
+        super.remove(index); 
+        super.add(getSortIndex(element), element);
+        return removed;
+    }
+
+    public int getSortIndex(T element ){
+        int index = 0;
+        for(int i  = 0; i < this.size()-1; i++){
+            if(element.compareTo(this.get(index+1))>0) index++;
+        }
+        return index;
+    }
     
 
 }
